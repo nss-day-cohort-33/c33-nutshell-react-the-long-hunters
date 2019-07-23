a58b77fc0cae53ddd0a4621538d8654f1c6d9f9a
Sydney Noh <sydneycnoh@gmail.com>
2019-07-23 09:18:28 -0500

    added semantic ui

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,27 +1,64 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'semantic-ui-css/semantic.min.css'
+import {
+    Container,
+    Dropdown,
+    Image,
+    Menu
+  } from 'semantic-ui-react'
 
 
 class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-                <ul className="nav nav-pills nav-fill">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">News</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/friends">Friends</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/messages">Messages</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/tasks">Tasks</Link>
-                    </li>
-                </ul>
-            </nav>
+
+        <Menu fixed='top' inverted>
+            <Container>
+                <Menu.Item as='a' header>
+                <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+                Project Name
+                </Menu.Item>
+                <Menu.Item as='a'>Home</Menu.Item>
+
+                <Dropdown item simple text='Dropdown'>
+                <Dropdown.Menu>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Header>Header Item</Dropdown.Header>
+                    <Dropdown.Item>
+                    <i className='dropdown icon' />
+                    <span className='text'>Submenu</span>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                        <Dropdown.Item>List Item</Dropdown.Item>
+                    </Dropdown.Menu>
+                    </Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                </Dropdown.Menu>
+                </Dropdown>
+            </Container>
+        </Menu>
+
+
+
+            // <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+            //     <ul className="nav nav-pills nav-fill">
+            //         <li className="nav-item">
+            //             <Link className="nav-link" to="/">News</Link>
+            //         </li>
+            //         <li className="nav-item">
+            //             <Link className="nav-link" to="/friends">Friends</Link>
+            //         </li>
+            //         <li className="nav-item">
+            //             <Link className="nav-link" to="/messages">Messages</Link>
+            //         </li>
+            //         <li className="nav-item">
+            //             <Link className="nav-link" to="/tasks">Tasks</Link>
+            //         </li>
+            //     </ul>
+            // </nav>
         )
     }
 }
