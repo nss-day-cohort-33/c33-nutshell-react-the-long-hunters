2d524c644ab8b3b61c4cc53c59fbec64038654b6
krystal gates <krystal_sabrina@yahoo.com>
2019-07-23 10:29:25 -0500

    set up nav bar and app views

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -15,50 +15,17 @@ class NavBar extends Component {
 
         <Menu fixed='top' inverted>
             <Container>
-                <Menu.Item as='a' header>
-                <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
-                Project Name
+                <Menu.Item as={Link} to="/" header>
+                <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />Dashboard
                 </Menu.Item>
-                <Menu.Item as='a'>Home</Menu.Item>
-
-                <Dropdown item simple text='Dropdown'>
+                <Dropdown item simple text=''>
                 <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Header Item</Dropdown.Header>
-                    <Dropdown.Item>
-                    <i className='dropdown icon' />
-                    <span className='text'>Submenu</span>
-                    <Dropdown.Menu>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                    </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/friends">Friends</Dropdown.Item>
+                    <Dropdown.Item as={Link} to="/logout">Logout</Dropdown.Item>
                 </Dropdown.Menu>
                 </Dropdown>
             </Container>
         </Menu>
-
-
-
-            // <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-            //     <ul className="nav nav-pills nav-fill">
-            //         <li className="nav-item">
-            //             <Link className="nav-link" to="/">News</Link>
-            //         </li>
-            //         <li className="nav-item">
-            //             <Link className="nav-link" to="/friends">Friends</Link>
-            //         </li>
-            //         <li className="nav-item">
-            //             <Link className="nav-link" to="/messages">Messages</Link>
-            //         </li>
-            //         <li className="nav-item">
-            //             <Link className="nav-link" to="/tasks">Tasks</Link>
-            //         </li>
-            //     </ul>
-            // </nav>
         )
     }
 }
